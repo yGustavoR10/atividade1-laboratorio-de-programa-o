99b4c9f62d6e62561c092d7380da1e067a918e48
Gustavo Resende Viana <gustavo.vianar10@gmail.com>
2026-08-31 19:27:46 -0300

    Delete LivroIndisponivelException.java

--- LivroIndisponivelException.java
+++
@@ -1,8 +0,0 @@
-package biblioteca;
-
-/** Exceção para livro indisponível. */
-public class LivroIndisponivelException extends BibliotecaException {
-    public LivroIndisponivelException(String mensagem) {
-        super(mensagem);
-    }
-}
