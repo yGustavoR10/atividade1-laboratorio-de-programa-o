2e3f2fb6d512b415884d96c526b956070ccec697
Gustavo Resende Viana <gustavo.vianar10@gmail.com>
2026-08-31 19:26:46 -0300

    Delete BibliotecaException.java

--- BibliotecaException.java
+++
@@ -1,8 +0,0 @@
-package biblioteca;
-
-/** Exceção personalizada da biblioteca. */
-public class BibliotecaException extends Exception {
-    public BibliotecaException(String mensagem) {
-        super(mensagem);
-    }
-}
