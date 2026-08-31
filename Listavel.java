551ab1d6b10beaf0eb2ee53dee428092373ef2c7
Gustavo Resende Viana <gustavo.vianar10@gmail.com>
2026-08-31 19:27:25 -0300

    Delete Listavel.java

--- Listavel.java
+++
@@ -1,9 +0,0 @@
-package biblioteca;
-
-/**
- * Interface responsável por definir um contrato
- * para classes que podem exibir informações.
- */
-public interface Listavel {
-    void exibirInformacoes();
-}
